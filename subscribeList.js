6f42628447484c16ec752440373af77bcdbe6f9e
Pak Hang Cheung <51543930+ApeironCheung@users.noreply.github.com>
2025-11-25 21:01:44 -0500

    fixed bugs of table not update

--- subscribeList.js
+++ subscribeList.js
@@ -3,6 +3,9 @@ const UPDATE_SUBSCRIPTIONS = {
         { 
             id: 'control-container', 
             getHtml: () => createControlHtml() 
+        },{
+            id: 'chart-display-container', 
+            getHtml: () => getFlyingStarChartHtml() 
         },
         {
             id: 'taishui-container', 
