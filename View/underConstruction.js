201c483ea712badf23ed08052cb27a05361e41ec
Pak Hang Cheung <51543930+ApeironCheung@users.noreply.github.com>
2026-03-30 22:40:34 -0400

    updated page under construction

--- View/underConstruction.js
+++ View/underConstruction.js
@@ -1,3 +1,6 @@
 export function pageUnderConstruction(){
-    return '<div><p>Page Under Construction</p></div>'
+    //return '<div><p>Page Under Construction</p></div>'
+    return `<div id="star-rigidbody">
+    <img src="./Data/selectIcon.png" class="rotating-img">
+</div>`
 }
